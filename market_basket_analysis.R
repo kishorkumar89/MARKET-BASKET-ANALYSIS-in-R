c0330936dd0af58fca1c98608ee98319d72497a8
unknown <shyam.kishor.kumar89@gmail.com>
2018-07-01 03:16:34 +0530

    R program Update

--- market_basket_analysis.R
+++ market_basket_analysis.R
@@ -1,9 +1,12 @@
+#set working directory
+setwd("D:/R/MARKET_BASKET_ANALYSIS")
+
 #read the excel data
 library(readxl)
-retail=read_excel("D:\\R\\MARKET_BASKET_ANALYSIS\\inputs\\Online_Retail.xlsx")
+retail=read_excel("inputs/Online_Retail.xlsx")
+
 #removes NAs or missing values from a data frame
 retail <- retail[complete.cases(retail), ]
-retail
 
 # baket Analysis Stated
 
@@ -16,31 +19,36 @@ itemList <- ddply(retail,c("CustomerID","Date"),
                   function(df1)paste(df1$Description, 
                                      collapse = ","))
 
-#write.csv(itemList,"D:\\R\\MARKET_BASKET_ANALYSIS\\market_basket.csv", quote = FALSE, row.names = TRUE)
 
 itemList$CustomerID <- NULL
 itemList$Date <- NULL
-
-#write.csv(itemList,"D:\\R\\MARKET_BASKET_ANALYSIS\\market_basket1.csv", quote = FALSE, row.names = TRUE)
 colnames(itemList) <- c("items")
 
-#write.csv(itemList,"D:\\R\\MARKET_BASKET_ANALYSIS\\market_basket2.csv", quote = FALSE, row.names = TRUE)
+write.csv(itemList,"output/market_basket2.csv", quote = FALSE, row.names = TRUE)
 
-summary(itemList)
 
-tr <- read.transactions('D:\\R\\MARKET_BASKET_ANALYSIS\\market_basket2.csv', format = 'basket', sep=',')
+tr <- read.transactions('output/market_basket2.csv', format = 'basket', sep=',')
 tr
 summary(tr)
 
 
 rules <- apriori(tr, parameter = list(supp=0.001, conf=0.8,maxlen=20))
 rules <- sort(rules, by='confidence', decreasing = TRUE)
-summary(rules)
-inspect(rules[1:10])
+#summary(rules)
+#inspect(rules[1:10])
 topRules <- rules[1:10]
+library(grid)
 library(arulesViz)
-plot(topRules)
+#plot(topRules)
 plot(topRules,method="graph",interactive=TRUE,shading=NA)
-plot(topRules, method="grouped")
+#plot(topRules, method="grouped")
 # rrules:
 write.csv(rules,"D:\\R\\MARKET_BASKET_ANALYSIS\\market_basket_RULES.csv", quote = FALSE, row.names = TRUE)
+
+plot(topRules,method="graph",interactive=TRUE,shading=NA)
+
+
+
+
+
+
